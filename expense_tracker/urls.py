6151f99c5ff439ff25e7668e6a30cf3dd2228a21
Keerthankumar Birisetti <keerthankumar09@gmail.com>
2025-09-15 22:05:03 +0530

    Update urls.py

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.dashboard, name="dashboard"), 
     path('', include('tracker.urls')),
 ]
 
